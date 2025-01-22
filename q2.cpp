b8acf19658066532711a881b9cf3de9ad2fd2016
Abhijeet-Rout-21 <arout1381@gmail.com>
2025-01-23 00:59:44 +0530

    ff

---
+++ q2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+ int addnunbers(int x,int y){
+    int result=x+y;
+    return reslut;
+
+ }
+ int main()
+ {
+    cout << addnunbers(43+65);
+    return 0;
+
+ }
