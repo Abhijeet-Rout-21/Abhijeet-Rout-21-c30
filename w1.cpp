068c6c791baad8cd07c570cbbb4fa418c88f2ba7
Abhijeet-Rout-21 <arout1381@gmail.com>
2025-01-27 01:38:51 +0530

    ww

---
+++ w1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+ int main(){
+    int c;
+    int total=o;
+    int num=0;
+ cout<<"entet thae number of age"<<endl;
+ cin c>>;
+ while(c=!2){
+    toatl=+c;
+    num++;
+    cout<<"enter the age"<<emdl;
+    cin>>c
+ }
+ cout<<"the total"<<total<<endl;
+ cout<<"thre AVG"<<toatl/num
+
+ }
